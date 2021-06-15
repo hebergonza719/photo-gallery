855567629da302f2509acd28b544e0fbc6e22087
Heber <hebergonza@gmail.com>
2021-06-15 02:13:41 -0400

    created front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import HomePage from '../components/HomePage';
+import Gallary from '../components/Gallary';
+import NotFoundPage from '../components/NotFoundPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <div className="main-content">
+        <Switch>
+          <Route component={HomePage} path="/" exact={true} />
+          <Route component={Gallary} path="/gallery" />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
     </div>
-  );
-}
+  </BrowserRouter>
+);
 
 export default App;
